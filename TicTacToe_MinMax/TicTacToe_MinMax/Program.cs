fea849b4fcdc2ffb2f4a025f48f425d02e18fdf4
GodOfAxolotl <70233758+GodOfAxolotl@users.noreply.github.com>
2021-04-28 09:10:45 +0200

    Add files via upload

---
+++ TicTacToe_MinMax/TicTacToe_MinMax/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TicTacToe_MinMax
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GameSession game = new GameSession();
+           
+          // game.automatedGame(8, true);
+            
+           game.setup();
+           game.loop();
+            
+        }
+    }
+}
